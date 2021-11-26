0ed20165af8b7e504524c7d6855e50492fe31bf6
Pavel Tigiev <paulo.tigiev@gmail.com>
2021-11-26 21:51:55 +0300

    add tab with all research data

--- src/views/Research.vue
+++ src/views/Research.vue
@@ -98,6 +98,9 @@
       background-color="primary"
       dark
     >
+      <v-tab @click="getResearchDataByIndicatorGroup(0)">
+        Все
+      </v-tab>
       <v-tab
         v-for="indicatorsGroup in indicatorsGroups"
         :key="indicatorsGroup.id"
@@ -108,6 +111,119 @@
     </v-tabs>
 
     <v-tabs-items v-model="tab" class="tabs-box">
+      <v-tab-item>
+        <v-card flat>
+          <v-dialog
+            v-model="rdDialog"
+            persistent
+            max-width="600px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                depressed
+                color="light-grey"
+                class="ma-4"
+                v-bind="attrs"
+                v-on="on"
+              >
+                + Добавить
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5"><h1 class="display-1">Новые данные</h1></span>
+              </v-card-title>
+              <v-divider class="mb-4"></v-divider>
+              <v-card-text class="pb-0">
+
+                <v-select
+                  :items="indicators.data"
+                  v-model="RDataItem.indicator_id"
+                  outlined
+                  label="Показатель"
+                  item-text="name"
+                  item-value="id"
+                ></v-select>
+                <v-text-field label="Значение" outlined v-model.number="RDataItem.value"></v-text-field>
+                <!-- <v-select
+                  :items="measurementUnits"
+                  v-model="RDataItem.measurement_unit_id"
+                  outlined
+                  label="Единица измерения"
+                  item-text="name"
+                  item-value="id"
+                ></v-select> -->
+
+              </v-card-text>
+              <v-card-actions class="pa-4">
+                <v-spacer></v-spacer>
+                <v-btn depressed color="light-grey" @click="closeRDataItem" class="mr-3">Закрыть</v-btn>
+                <v-btn depressed color="light-grey" @click="createRDataItem" class="mr-3">Создать</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+          <!-- <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                depressed
+                color="light-grey"
+                class="ma-4 ml-0"
+                v-bind="attrs"
+                v-on="on"
+              >
+                Еще
+                <v-icon right>
+                  mdi-chevron-down
+                </v-icon>
+              </v-btn>
+            </template>
+            <v-list dense>
+              <v-list-item-group>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title @click="openIndicatorDialog">Добавить показатель</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-menu> -->
+
+          <v-progress-linear indeterminate v-if="loading"></v-progress-linear>
+          <v-container class="tree-box" fluid>
+            <v-row class="tree-header">
+              <v-col cols="2">
+                Номер
+              </v-col>
+              <v-col cols="4">
+                Показатель
+              </v-col>
+              <v-col cols="3">
+                Значениие показателя
+              </v-col>
+              <v-col cols="3">
+                Ед.Измерения
+              </v-col>
+            </v-row> 
+
+            <v-row class="tree-row tree-folders" v-for="item in researchDataByIndicatorGroup" :key="item.id">
+              <v-col cols="2">
+                {{ item.indicator_id }}
+              </v-col>
+              <v-col cols="4">
+                {{ item.indicator_name }}
+              </v-col>
+              <v-col cols="3">
+                {{ item.value }}
+              </v-col>
+              <v-col cols="3">
+                {{ item.measurement_name }}
+              </v-col>
+            </v-row>
+          </v-container>
+
+        </v-card>
+      </v-tab-item>
       <v-tab-item
         v-for="indicatorsGroup in indicatorsGroups"
         :key="indicatorsGroup.id"
@@ -312,7 +428,11 @@ export default {
       this.$store.dispatch('getAcceptances')
     },
     getResearchDataByIndicatorGroup (group_id) {
-      this.researchDataByIndicatorGroup = this.researchData.filter(element => element.indicator_group_id == group_id)
+      if (group_id == 0) {
+        this.researchDataByIndicatorGroup = this.researchData
+      } else {
+        this.researchDataByIndicatorGroup = this.researchData.filter(element => element.indicator_group_id == group_id)
+      }
     },
     getIndicators () {
       this.$store.dispatch('getIndicators')
@@ -398,12 +518,12 @@ export default {
     },
     researchData () {
       setTimeout(() => {
-        this.getResearchDataByIndicatorGroup(this.indicatorsGroups[0].id)
+        this.getResearchDataByIndicatorGroup(0)
       }, 200)
     },
     acceptances () {
       this.acceptances.forEach(acceptance => {
-        acceptance.acceptance_label = this.$moment.utc(acceptance.acceptance_date).format('YYYY.MM.DD - hh:mm') + ' : ' + acceptance.contractor
+        acceptance.acceptance_label = acceptance.uuid + " от " + this.$moment.utc(acceptance.acceptance_date).format('YYYY.MM.DD')
       })
     }
   }
